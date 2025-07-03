915f3e2f72af4c5280cba582f625fa568cb77c3d
bukunmikuti <bukunmikuti@gmail.com>
2025-07-03 16:11:10 +0100

    remove all files

--- js/core.js
+++
@@ -1,9 +0,0 @@
-
-export let $ = (sel, type=undefined) => {
-	if (type == 'id')
-	return document.getElementById(sel);
-	if (type == 'class')
-	return document.getElementsByClassName(sel);
-	if (!type)
-	return document.querySelector(sel)
-}
